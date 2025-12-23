ae12a2728a839e02d922945d2dd129ca2d0f48c8
Saurabh3499 <saurabhbhojane4@gmail.com>
2025-12-23 16:28:23 +0530

    Update app.py

--- order-service/app.py
+++ order-service/app.py
@@ -6,13 +6,11 @@ app = Flask(__name__)
 @app.route("/order")
 def order():
     cart = requests.get("http://cart-service:8080/cart").json()
-
     return {
         "order_id": 101,
-        "status": "CONFIRMED",
-        "cart_details": cart
+        "items": cart["cart"],
+        "final_price": cart["total"]
     }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
