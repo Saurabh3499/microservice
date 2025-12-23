5706a417d630f5540a407641d72022c4c9536989
Saurabh3499 <saurabhbhojane4@gmail.com>
2025-12-23 16:29:13 +0530

    Update app.py

--- cart-service/app.py
+++ cart-service/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+cart = {}
 
-@app.route("/cart")
-def cart():
-    return {
-        "cart": ["Milk", "Bread"],
-        "total": 120
-    }
+@app.route("/cart", methods=["GET"])
+def get_cart():
+    total = sum(v["price"] * v["qty"] for v in cart.values())
+    return {"cart": cart, "total": total}
+
+@app.route("/cart", methods=["POST"])
+def add_to_cart():
+    item = request.json
+    name = item["name"]
+
+    if name in cart:
+        cart[name]["qty"] += 1
+    else:
+        cart[name] = {
+            "price": item["price"],
+            "qty": 1
+        }
+    return jsonify({"message": "added", "cart": cart})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
