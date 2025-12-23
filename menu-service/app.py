ab1f85d623d2c45440f54eab6cb713a3dd1f2e4e
Saurabh3499 <saurabhbhojane4@gmail.com>
2025-12-23 16:30:26 +0530

    Update app.py

--- menu-service/app.py
+++ menu-service/app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 @app.route("/menu")
 def menu():
     return {
-        "items": ["Rice", "Milk", "Bread", "Vegetables"]
+        "items": [
+            {"name": "Rice", "price": 50, "image": "https://via.placeholder.com/100?text=Rice"},
+            {"name": "Milk", "price": 30, "image": "https://via.placeholder.com/100?text=Milk"},
+            {"name": "Banana", "price": 10, "image": "https://via.placeholder.com/100?text=Banana"}
+        ]
     }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
